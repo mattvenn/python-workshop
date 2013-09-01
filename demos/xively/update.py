40318e4ef1c5301f5af5d8c60f06baec516dd53a
mattvenn <matt@mattvenn.net>
2013-09-01 21:45:09 +0100

    demo updated to use epoch seconds for win compatibility

--- demos/xively/update.py
+++ demos/xively/update.py
@@ -14,12 +14,11 @@ feed_id = "120508"
 pfu = CosmFeedUpdate(feed_id,key)
 
 #open a special file that stores how long the raspberry has been running for
-f=open("/proc/uptime","r");
-uptime_string=f.readline()
-uptime=uptime_string.split()[0]
-pfu.addDatapoint('uptime', uptime)
+
+epoch = time.time()
+pfu.addDatapoint('epoch', epoch)
 
 # finish up and submit the data
 pfu.buildUpdate()
 pfu.sendUpdate()
-print "sent update", uptime
+print "sent update to https://xively.com/feeds/120508", epoch
