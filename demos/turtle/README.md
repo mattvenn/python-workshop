c38b0298fa5ba5416337cfa5ae67e58b20ecdd11
Matt Venn <matt@mattvenn.net>
2013-09-10 18:10:57 +0100

    better turtle demo

--- demos/turtle/README.md
+++ demos/turtle/README.md
@@ -1,1 +1,12 @@
-http://openbookproject.net/thinkcs/python/english3e/hello_little_turtles.html
+# Turtle demo!
+
+Do you remember using logo? This library that comes with python allows easy shape drawing and lets us start trying out the important parts of python straight away.
+
+# Tutorials
+
+* Nice interactive course: http://interactivepython.org/courselib/static/thinkcspy/PythonTurtle/helloturtle.html
+* Another very good tutorial: http://openbookproject.net/thinkcs/python/english3e/hello_little_turtles.html
+
+# Documentation
+
+All the gory details: http://docs.python.org/2/library/turtle.html
--- demos/turtle/turtle_demo.py
+++ demos/turtle/turtle_demo.py
@@ -1,9 +1,16 @@
-import turtle             # Allows us to use turtles
-wn = turtle.Screen()      # Creates a playground for turtles
-alex = turtle.Turtle()    # Create a turtle, assign to alex
+import turtle                   # Allows us to use turtles
+wn = turtle.Screen()            # Creates a playground for turtles
+my_turtle = turtle.Turtle()     # Create a turtle, assign to my_turtle
+my_turtle.shape('turtle')       # make it look like a turtle
 
-alex.forward(50)          # Tell alex to move forward by 50 units
-alex.left(90)             # Tell alex to turn by 90 degrees
-alex.forward(30)          # Complete the second side of a rectangle
+my_turtle.forward(50)           # Tell my_turtle to move forward by 50 units
+my_turtle.left(90)              # Tell my_turtle to turn by 90 degrees
+my_turtle.color("blue")         # change the colour
+my_turtle.forward(30)           # Complete the second side of a rectangle
+my_turtle.penup()               # lift the pen
+my_turtle.forward(30)
+my_turtle.pendown()             # lower the pen
+my_turtle.pensize(5)            # increase pen size
+my_turtle.forward(30)
 
-turtle.mainloop()  
+turtle.mainloop()               # wait
