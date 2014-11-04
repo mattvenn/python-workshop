b3e3b9a11e9a4083876df75b8447c3b7fac00d7c
matt venn <matt@mattvenn.net>
2014-11-04 16:32:28 +0000

    comments and readme

---
+++ demos/pygame/snake/README.md
@@ -0,0 +1,74 @@
+# Snake example
+
+This is a port of the classic snake game found on Nokia phones. I'm pretty sure
+everyone's seen it or played it!
+
+# Game setup
+
+The game starts by defining some constants that choose the size of the sprites
+and the size of the board. 
+
+We define some colours so we can use then easily later.
+
+Then we define our objects.
+
+# Objects Overview
+
+In this game we use 3 objects:
+
+* Snake - this is the head of the snake, that we control
+* Body - the body grows as we eat food, it follows the head
+* Food - randomly appears and after a while disappears
+
+## Snake
+
+The snake object is the most complicated. It can move around, has a sound for
+when it eats, and can add extra body sections to itself. 
+
+In the update method, it:
+
+* moves the head, 
+* moves all the body sections so they follow the head 
+* check to see if we crashes into any of the body sections
+* checks to see if it hits any food.
+
+If any food is hit, the `add_body()` method is called. This method adds a new
+body sprite to the snake, and also adds the sprite to the snake group and the
+all_sprites group. This lets us check if we ever crash into ourselves.
+
+## Body
+
+The body object just loads the image and can then have its position queried and
+updated.
+
+## Food
+
+The food object creates a new food sprite, and puts it somewhere randomly on the
+screen. It makes a note of the time when it was created, and after a certain
+time, the update() method will remove the sprite so it disappears.
+
+# More game setup
+
+After the objects have been defined, we've still got some stuff to do before we
+can get started:
+
+* Initialise pygame with the init() method
+* Set the screen size
+* Create the sprite groups
+* Load sounds
+* Create the player object
+* Start the clock
+
+# The main loop
+
+The loop is the heart of the game. The most important thing here is to regularly
+call update() on all the sprites.
+
+We also:
+
+* Check for key presses
+* Make food appear randomly
+* Wipe the screen and redraw all the sprites
+* Show the score
+* Check to see if the snake has crashed, and if so, end the game.
+
--- demos/pygame/snake/snake.py
+++ demos/pygame/snake/snake.py
@@ -23,41 +23,42 @@ RED      = (255, 0,   0  )
  
 # Screen dimensions
 tile_size = 32 #size of the sprites
-tiles = 20
+tiles = 15 #how big the board should be
+score_height = tile_size #how big the score header should be
+
+#then work out the screen height and width
 screen_width  = tile_size * tiles
-score_height = tile_size
 screen_height = tile_size * tiles + score_height
-start_speed = 10
 
 #points stuff
 chomp_points = 1 # this also gets added to the speed of the game
 food_timer = 3000 # how long food last before disappearing (ms)
 
 # The snake object
-class Player(pygame.sprite.Sprite):
+class Snake(pygame.sprite.Sprite):
  
     # Constructor function
     def __init__(self):
         # Call the parent's constructor
         pygame.sprite.Sprite.__init__(self)
  
+        #load the eating sound
         self.eat_sound = pygame.mixer.Sound('eat.wav') 
         self.eat_sound.set_volume(0.7)
 
-        # Set height, width
+        #load the image
         self.imageMaster = pygame.image.load("head.png").convert_alpha()
         self.image = self.imageMaster
 
+        #set the starting position
         self.rect = self.image.get_rect()
         self.rect.y = (tiles / 2) * tile_size
         self.rect.x = (tiles / 2) * tile_size
 
+        #initial starting setup
         self.points = 0        
         self.direction = 'left'
         self.done = False
-
-    def get_xy(self):
-        return (self.rect.x,self.rect.y)
 
     #add a new body part
     def add_body(self,number):
@@ -65,10 +66,11 @@ class Player(pygame.sprite.Sprite):
         snake_list.add(new_body)
         all_sprite_list.add(new_body)
 
+    #this is called every frame
     def update(self):
     
         old_xy = (self.rect.x,self.rect.y)
-        """ Update the player position. """
+        """ Update the snake position. """
         # Move left/right
         if self.direction == 'left':
             self.image = pygame.transform.rotate(self.imageMaster, 90)
@@ -125,116 +127,125 @@ class Body(pygame.sprite.Sprite):
         #load sprite
         self.image = pygame.image.load("tail.png").convert_alpha()
 
+        #starting position
         self.rect = self.image.get_rect()
         (self.rect.x,self.rect.y) = (x,y)
-        #self.rect.y = y
 
+    #return the position of the body
     def get_xy(self):
         return (self.rect.x,self.rect.y)
 
+    #update the position
     def set_xy(self,(x,y)):
         self.rect.x = x
         self.rect.y = y
         
+#food objects
 class Food(pygame.sprite.Sprite):
     def __init__(self):
 
         # Call the parent's constructor
         pygame.sprite.Sprite.__init__(self)
 
-        #load sprite
+        #load sprite's image
         self.image = pygame.image.load("fruit.png").convert_alpha()
  
         # put it somewhere random
         self.rect = self.image.get_rect()
-        #but avoid the top score header
+        #but avoid the top score snakeer
         self.rect.y = random.randint(0,tiles-1) * tile_size + score_height
         self.rect.x = random.randint(0,tiles-1) * tile_size
 
+        #make a note of the time
         self.time = pygame.time.get_ticks()
 
-    def get_xy(self):
-        return (self.rect.x,self.rect.y)
-
+    #called every frame
     def update(self):
         if pygame.time.get_ticks() - self.time > food_timer:
             #disappear
             self.kill()
- 
-# Call this function so the Pygame library can initialize itself
-pygame.init()
- 
-# Create sized screen
-screen = pygame.display.set_mode([screen_width, screen_height])
- 
-# Set the title of the window
-pygame.display.set_caption('Snake')
 
-#all_sprite_list = pygame.sprite.Group()
-snake_list = pygame.sprite.Group()
-all_sprite_list = pygame.sprite.Group()
-food_list = pygame.sprite.Group()
-
-#show the score
+#function to show the score
 def show_score():
 
     font = pygame.font.SysFont('Arial', tile_size - 5)
     background = pygame.Surface((screen_width,score_height))
     background = background.convert()
     background.fill(BLUE)
-    text = font.render("Points = %d" % player.points, 1, BLACK)
+    text = font.render("Points = %d" % snake.points, 1, BLACK)
     textpos = text.get_rect(centerx=screen_width/2,centery=score_height / 2)
     background.blit(text, textpos)
     screen.blit(background, (0,0))
 
+"""
+Now all the objects have been defined, we can start up the game
+"""
+
+# Call this function so the Pygame library can initialize itself
+pygame.init()
  
-# Create the player 
-player = Player()
-all_sprite_list.add(player)
-
-clock = pygame.time.Clock()
+# Create screen
+screen = pygame.display.set_mode([screen_width, screen_height])
  
-done = False
+# Set the title of the window
+pygame.display.set_caption('Snake')
 
-print("starting")
+#we keep a track of the sprites in these groups:
+#the head and the body of the snake in here
+snake_list = pygame.sprite.Group()
+#food in here
+food_list = pygame.sprite.Group()
+#everything in here
+all_sprite_list = pygame.sprite.Group()
 
+#load the food appear sound
 food_sound = pygame.mixer.Sound('appear.wav') 
 food_sound.set_volume(0.7)
 
 end_sound = pygame.mixer.Sound('game-over.wav') 
 end_sound.set_volume(0.7)
 
+# Create the snake object, and add to group
+snake = Snake()
+all_sprite_list.add(snake)
+
+#start clock
+clock = pygame.time.Clock()
+ 
+print("starting")
+
+#when this changes to true, the game is over
+done = False
+
 while not done:
     #we could get multiple events here. So that's why we break if a key is pressed.
-    #it stops the player from being able to change directions multiple times in one update
+    #it stops the snake from being able to change directions multiple times in one update
     for event in pygame.event.get():
         #allow click on window close button
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and player.direction != 'right':
-                player.direction = 'left'
+            if event.key == pygame.K_LEFT and snake.direction != 'right':
+                snake.direction = 'left'
                 break
-            elif event.key == pygame.K_RIGHT and player.direction != 'left':
-                player.direction = 'right'
+            elif event.key == pygame.K_RIGHT and snake.direction != 'left':
+                snake.direction = 'right'
                 break
-            elif event.key == pygame.K_UP and player.direction != 'down':
-                player.direction = 'up'
+            elif event.key == pygame.K_UP and snake.direction != 'down':
+                snake.direction = 'up'
                 break
-            elif event.key == pygame.K_DOWN and player.direction != 'up':
-                player.direction = 'down'
+            elif event.key == pygame.K_DOWN and snake.direction != 'up':
+                snake.direction = 'down'
                 break
     
     #make some food randomly
     if random.randint(0,20) == 0:
         food = Food()
-        crash = False
-        #if the sprite happens to be on something already, don't add it
-        for sprite in all_sprite_list:
-            if food.get_xy() == sprite.get_xy():
-                crash = True
-        #otherwise add it to the lists
-        if not crash:
+        #the food appears in a random place, but we don't want it to appear on the 
+        #snake itself. So check if it collides with anything and if so, don't add 
+        #it to the food list.
+        food_hit_list = pygame.sprite.spritecollide(food, all_sprite_list, False)
+        if len(food_hit_list) == 0:
             food_sound.play()
             all_sprite_list.add(food)
             food_list.add(food)
@@ -242,26 +253,29 @@ while not done:
     #update all sprites
     all_sprite_list.update()
  
+    #blank the screen
     screen.fill(WHITE)
  
+    #draw all sprites
     all_sprite_list.draw(screen)
 
     #show scores
     show_score()
 
     #crashed?
-    if player.done:
-        #let player see the crash
+    if snake.done:
+        #let snake see the crash
         end_sound.play()
         pygame.time.wait(2000)
         done = True
 
+    #flip the screen, this prevents flickering (look up double buffering)
     pygame.display.flip()
  
     #slow update, but increases with points
     #argument to tick is the framerate
-    clock.tick(5 + player.points)
+    clock.tick(5 + snake.points)
 
-print("you got %d points" % player.points)
-
+#after crashed, show points and quit
+print("you got %d points" % snake.points)
 pygame.quit()
