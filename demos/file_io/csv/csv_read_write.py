2a165af928b7c74ed26f0affc3553efd7bf926ca
matt venn <matt@mattvenn.net>
2015-05-26 11:56:50 +0100

    reading

--- demos/file_io/csv/csv_read_write.py
+++ demos/file_io/csv/csv_read_write.py
@@ -19,5 +19,29 @@ while record < records:
     record = record + 1
 
 fh.close()
-# now we'll read the data back in
 
+# now we'll read the data back in
+fh = open('csv.txt')
+# read the lines
+records = fh.readlines()
+# get 2 lists ready to store the info
+names = []
+scores = []
+# loop through all the lines in the file
+for record in records:
+    # get rid of the trailing \n at the end of the file
+    record = record.strip()
+
+    # split the line into the parts
+    name, score = record.split(',')
+
+    # add the name and score to the lists
+    names.append(name)
+
+    # turn the score back to an integer
+    score = int(score)
+    scores.append(score)
+
+# print the data
+print(names)
+print(scores)
